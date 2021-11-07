6738d8c4814bff5d264b067c5291eacba3ab3523
MAKSIM LISTSENKO <m.listsenko.20@abdn.ac.uk>
2021-11-07 05:13:42 +0000

    done tasks 2.1, 2.2, 2.3

--- maksim_listsenko/src/main/java/uoa/assignment1/game/Map.java
+++ maksim_listsenko/src/main/java/uoa/assignment1/game/Map.java
@@ -6,14 +6,44 @@ import uoa.assignment1.character.Player;
 
 public class Map {
 
-public String [][] layout;
-public GameCharacter characters [] ;
+    public String [][] layout;
+    public GameCharacter characters [] = new GameCharacter[4];
  
-  Map (int height, int width) {
-	 
-  }
- 
-  public void printLayout() {
-	
-   }
+    Map (int height, int width) {
+        this.layout = new String[height][width];
+        initialiseArray();
+        Player player = new Player("Player");
+        player.row = height - 1;
+        player.column = width - 1;
+        characters[0] = player;
+        Monster monster1 = new Monster("Monster1");
+        monster1.row = 0;
+        monster1.column = width - 1;
+        characters[1] = monster1;
+        Monster monster2 = new Monster("Monster2");
+        monster2.row = height - 1;
+        monster2.column = 0;
+        characters[2] = monster2;
+        Monster monster3 = new Monster("Monster3");
+        monster3.row = 0;
+        monster3.column = 0;
+        characters[3] = monster3;
+    }
+
+    private void initialiseArray() {
+        for (int i = 0; i < layout.length; i++) {
+            for (int j = 0; j < layout[i].length; j++) {
+                layout[i][j] = ".";
+            }
+        }
+    }
+
+    public void printLayout() {
+        for (int i = 0; i < layout.length; i++) {
+            for (int j = 0; j < layout[i].length; j++) {
+                System.out.print(layout[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
